03ee2dc5220c6ebe8f1cc561ed0ca175de617e77
Skatl <3050802189@qq.com>
2021-09-17 20:21:12 +0800

    Frist Commit

---
+++ Project2_Interrupt/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+int interruptPin = 2;
+int ledpin = 13;
+int period = 500;
+void gofast();
+void setup()
+{
+
+  pinMode(ledpin, OUTPUT);
+  pinMode(interruptPin, OUTPUT);
+  digitalWrite(interruptPin, HIGH);
+  attachInterrupt(0, gofast, FALLING);
+}
+
+void loop()
+{
+  digitalWrite(ledpin, HIGH);
+  delay(period);
+  digitalWrite(ledpin, LOW);
+  delay(period);
+}
+
+void gofast()
+{
+  period = 100;
+}
